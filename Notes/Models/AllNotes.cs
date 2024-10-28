95efd28051399cb8f607b2709e43dc83f227641c
werley.souza <werley.souza@maxdata.com.br>
2024-10-28 09:36:42 -0300

    Adicionando Pages

---
+++ Notes/Models/AllNotes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Models
+{
+    internal class AllNotes
+    {
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+        public AllNotes() => LoadNotes();
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            IEnumerable<Note> notes = Directory
+                                    .EnumerateFiles(appDataPath, "*.notes.txt")
+                                    .Select(filename => new Note()
+                                    {
+                                        Filename = filename,
+                                        Text = File.ReadAllText(filename),
+                                        Date = File.GetLastWriteTime(filename)
+                                    })
+                                    .OrderBy(x => x.Date);
+            foreach (Note note in notes)
+                Notes.Add(note);
+        }
+    }
+}
